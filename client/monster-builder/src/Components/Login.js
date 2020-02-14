fbe82f49c135926392e6688f550f6899bf21124c
johnpham47 <johnpham47@gmail.com>
2020-02-14 12:09:27 -0600

    Completed invalid credential message

--- client/monster-builder/src/Components/Login.js
+++ client/monster-builder/src/Components/Login.js
@@ -4,6 +4,7 @@ import { useState } from 'react'
 
 const Login = () => {
     const [user, setUser] = useState({})
+    const [error, setError] = useState(1)
 
     function handleInput(e) {
         setUser({
@@ -24,10 +25,23 @@ const Login = () => {
                 password: user.password
             })
         })
+        .then(response => response.json())
+        .then(auth => {
+            if (auth.isAuthenticated) {
+                // Redux here
+            }
+            else {
+                setError({
+                    message: 2
+                })
+            }
+
+        })
     }
     return (
         <div>
             <h1>Login</h1>
+            {error.message === 2 && <p>Invalid credentials, please try again.</p>}
             <label>Username</label>
             <input onChange={handleInput} type="text" name="username" />
             <label>Password</label>
