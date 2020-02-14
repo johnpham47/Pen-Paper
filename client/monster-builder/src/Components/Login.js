5646d3aa5cca006201094b5bdf6842e347612b99
johnpham47 <johnpham47@gmail.com>
2020-02-14 10:58:37 -0600

    Setting up login input handler

--- client/monster-builder/src/Components/Login.js
+++ client/monster-builder/src/Components/Login.js
@@ -3,13 +3,23 @@ import { connect } from 'react-redux'
 import { useState } from 'react'
 
 const Login = () => {
+    const [user, setUser] = useState({})
+
+    function handleInput(e) {
+        setUser({
+            ...user,
+            [e.target.name] : e.target.value
+        })
+    }
+
     return (
         <div>
             <h1>Login</h1>
             <label>Username</label>
-            <input type="text" name="username" />
+            <input onChange={handleInput} type="text" name="username" />
             <label>Password</label>
-            <input type="text" name="password" />
+            <input onChange={handleInput} type="text" name="password" />
+            <button>Login</button>
         </div>
     )
 }
